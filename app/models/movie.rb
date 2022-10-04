c2fd18e7b063d48fee436379dbb8e25c7ede11a0
Xiaomeng Li <xl3130@columbia.edu>
2022-10-04 19:26:41 +0000

    part1 try1

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,21 @@
 class Movie < ActiveRecord::Base
+
+  def self.all_ratings
+    ['G', 'PG', 'PG-13','R']
+  end
+
+  def self.check_ratings(p)
+    if p[:ratings].nil? || p[:ratings] == []
+      return []
+    end
+
+    return p[:ratings].keys
+  end
+
+  def self.with_ratings(ratings_list)
+    if ratings_list.nil? || ratings_list == []
+      return Movie.a;;
+    else
+      return Movie.where(rating: ratings_list)
+    end
 end
