e13d3e4bb1263e5363caaae2aa2384f343313b35
Xiaomeng Li <xl3130@columbia.edu>
2022-10-06 04:35:17 +0000

    done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,54 +7,62 @@ class MoviesController < ApplicationController
   end
 
   def index
+    # @movies = Movie.all
 
     @redirect_flag = false
+
+    if session.key?(:ratings) && !params.key?(:ratings)
+      params[:ratings] = session[:ratings]
+      @redirect_flag = true
+    end
+
+    if session.key?(:sort) && params[:commit] != "Refresh" && !params.key?(:sort)
+      params[:sort] = session[:sort]
+      @redirect_flag = true
+    end
+
+    if @redirect_flag == true
+      redirect_to movies_path(:ratings => params[:ratings], :sort => params[:sort])
+    end
 
     if !params.key?(:ratings)
       params[:ratings] = Hash[Movie.all_ratings.collect { |item| [item, "1"] } ]
     end
 
-    if session.key?(:ratings) && !params.key?(:ratings)
-      @redirect_flag = true
-      params[:ratings] = session[:ratings]
-    end
-
-    if session.key?(:sort) && params[:commit] != "Refresh" && !params.key?(:sort)
-      @redirect_flag = true
-      params[:sort] = session[:sort]
-    end
-
-    if @redirect_flag
-      redirect_to movies_path(:ratings => params[:ratings], :sort => params[:sort])
-    end
-
     
 
     @all_ratings = Movie.all_ratings
+    # @ratings_to_show = params[:ratings].nil? ? [] : params[:ratings].keys
     @ratings_to_show = Movie.check_ratings(params)
-    
+    # @movies_to_show = Movie.with_ratings(@ratings_to_show, order)
+
     if (session.key?(:sort) && params[:commit] == "Refresh")
       params[:sort] = session[:sort]
     end
+
+    # params[:sort] = session[:sort]
     
-    if !params.key?(:sort) 
-      @movies_to_show = Movie.with_ratings(@ratings_to_show)
-      @title_bg = ''
-      @date_bg = ''
-    else
+    
+    if params.key?(:sort) 
       if params[:sort] == 'title'
-        @movies_to_show = Movie.with_ratings(@ratings_to_show)
+        @movies_to_show = Movie.with_ratings(@ratings_to_show, :title)
         @title_bg = 'hilite'
         @date_bg = ''
       else
-        @movies_to_show = Movie.with_ratings(@ratings_to_show)
+        @movies_to_show = Movie.with_ratings(@ratings_to_show, :release_date)
         @date_bg = 'hilite'
         @title_bg = ''
       end
+    else
+      @movies_to_show = Movie.with_ratings(@ratings_to_show)
+      @title_bg = ''
+      @date_bg = ''
     end
 
     if params.key?(:sort) && params[:commit] != "Refresh"
       session[:sort] = params[:sort]
+    # else
+    #   session.delete(:sort)
     end
 
     session[:ratings] = params[:ratings]
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,40 +1,43 @@
 class Movie < ActiveRecord::Base
 
-  def self.all_ratings
-      ['G','PG','PG-13','R']
-  end
+    def self.all_ratings
+        ['G','PG','PG-13','R']
+    end
 
-  def self.check_ratings(param)
+    def self.check_ratings(par)
 
-      if param[:ratings].nil? || param[:ratings]==[]
-          return []
-      end
-      
-      return param[:ratings].keys
-  end
+        if par[:ratings].nil? || par[:ratings] == []
+            return []
+        end
+        
+        return par[:ratings].keys
+    end
 
-  def self.with_ratings(ratings_list, order = nil)
+    def self.with_ratings(ratings_list, order = nil)
+        # if ratings_list is an array such as ['G', 'PG', 'R'], retrieve all
+        #  movies with those ratings
+        # if ratings_list is nil, retrieve ALL movies
+        if order.nil?
+            if ratings_list.nil? || ratings_list == []
+                return Movie.all
+            else
+                return Movie.where(rating: ratings_list)
+            end
+        else
+            if ratings_list.nil? || ratings_list == []
+                if order == :title
+                    return Movie.all.order(:title)
+                else
+                    return Movie.all.order(:release_date)
+                end
+            else
+                if order == :title
+                    return Movie.where(rating: ratings_list).order(:title)
+                else
+                    return Movie.where(rating: ratings_list).order(:release_date)
+                end
+            end
+        end
 
-      if order.nil?
-          if ratings_list.nil? || ratings_list == []
-              return Movie.all
-          else
-              return Movie.where(rating: ratings_list)
-          end
-      else
-          if ratings_list.nil? || ratings_list == []
-              if order == :title
-                  return Movie.all.order(:title)
-              else
-                  return Movie.all.order(:release_date)
-              end
-          else
-              if order == :title
-                  return Movie.where(rating: ratings_list).order(:title)
-              else
-                  return Movie.where(rating: ratings_list).order(:release_date)
-              end
-          end
-      end
-  end
+    end
 end
